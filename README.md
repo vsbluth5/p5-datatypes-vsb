a296c9ac0159d36bb21a71f3ea9bd8b28640a9c6
Glitch (woolly-spectrum-keeper) <none>
2021-06-01 15:27:39 +0000

    Import of upperlinecode/data-types-javascript

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Get Started!
+# Data Types and Variables
 
-Add files and start typing 🎉
+[![Run on Repl.it](https://repl.it/badge/github/upperlinecode/data-types-javascript)](https://repl.it/github/upperlinecode/data-types-javascript)
+
+## The Lab
+
+Since these are foundational skills for the rest of the course, let's spend a few minutes working with variables and identifying data types, in the context of describing a pet.
+
+Open up challenges.js and answer each of the querstions there.
---
+++ challenges.js
@@ -0,0 +1,44 @@
+// Do either you or your partner have a pet? If so, you'll be describing them. Otherwise, have fun inventing an animal together.
+
+
+// For each of the following questions, store the answer in a variable. 
+// Then, fill in the blank in the comment with the data type you've used.
+// Finally, console.log a message using that variable.
+
+// === EXAMPLE ===
+// 0. How many times does your pet eat each day?
+const meals = 2;
+// The data type is __number__.
+console.log("My pet eats " + meals + " meals a day.");
+
+
+// 1. What is the pet's name?
+// The data type is ___.
+// const ___ = ___
+
+
+// 2. How old is the pet?
+// The data type is ___.
+
+
+// 3. What type of animal is the pet?
+// The data type is ___.
+
+
+// 4. Is the pet female?
+// The data type is ___.
+
+
+// 5. Approximately how much do you think the pet weighs?
+// The data type is ___.
+
+
+
+
+// You shouldn't need it often, but JavaScript has a neat built-in function called "typeof" which can tell you the data type stored in a variable.
+// For example, if you call typeof("koala"), JavaScript will give you "string."
+
+
+// Check your answers now with five call to console.log, which print statements of the form "Penelope is a string."
+
+// console.log(___ + " is a " + typeof())
